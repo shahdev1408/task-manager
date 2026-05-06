a849798df0626281b9e031deb234af2744485228
Dev Shah <shahdev1408@gmail.com>
2026-05-06 08:55:23 +0530

    fix production cookies

--- server/utils/index.js
+++ server/utils/index.js
@@ -7,8 +7,8 @@ const createJWT = (res, userId) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    secure: true,
+    sameSite: "none",
     maxAge: 1 * 24 * 60 * 60 * 1000,
   });
 };
